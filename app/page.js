2d5c91908f97e31497c09193ea824394bdc77473
Segun Ajibola <ajbl.segun@gmail.com>
2023-12-18 10:00:22 +0100

    fixKey

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
@@ -6,28 +6,42 @@ import Input from "@/components/Input";
 import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [images, setImages] = useState([]);
+  const [term, setTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [images, setImages] = useState([])
-  const [term, setTerm] = useState()
-  const [isLoading, setIsLoading] = useState(false)
-  
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
-      .then(res => res.json())
-      .then(data => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`[ERROR ${res.status}] ${res.statusText}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data);
         setImages(data.hits);
         setIsLoading(false);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, [term]);
 
   return (
     <div className="bg-red-300 h-screen">
       <Navbar />
       <Input />
-      {images && images?.map(image => (
-          <div>
-            <img src={image.src} />
+      {isLoading && <p>Loading...</p>}
+      {images &&
+        images.map((image) => (
+          <div key={image.id}>
+            <Image
+              src={image.webformatURL}
+              alt={image.tags}
+              width={image.webformatWidth}
+              height={image.webformatHeight}
+            />
           </div>
         ))}
     </div>
